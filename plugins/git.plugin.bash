8332627bd8402fb212b66cd3babf5fb2920ea9cc
victorbrca <victorbrca@yahoo.ca>
2018-10-01 13:29:36 -0400

    Adding arguments to git-update

--- plugins/git.plugin.bash
+++ plugins/git.plugin.bash
@@ -20,8 +20,10 @@ alias gitpushom='git push -u origin master'
 # help:git-update:Adds, commits and push files on a Git repo 
 git-update ()
 {
-  local Green Yellow Red Color_Off UYellow UGreen Blinking_White Blinking_Red
-    
+  local Green Yellow Red Color_Off UYellow UGreen Blinking_White Blinking_Red \
+  commit_file commit_message
+
+  usage="git-update [-h]{commit} {file}"
   Red='\e[0;31m'
   Yellow='\e[0;33m'
   Green='\e[0;32m'
@@ -30,9 +32,36 @@ git-update ()
   Blinking_White='\e[5;37;40m'
   Blinking_Red='\e[5;31;40m'
   Color_Off='\e[0m'
-    
-  echo -e "\n${Blinking_White}Updating git Repo${Color_Off}"
-    
+
+  if [[ $# -eq 0 ]] ; then
+    commit_message="$(date)"
+  elif [[ $# -eq 1 ]] ; then
+    if [[ "$1" == "-h" ]] ; then
+      echo "$usage"
+      return 0
+    elif [ -f "$1" ] ; then
+      commit_file="$1"
+    else
+      commit_message="$1"
+  elif [[ $# -eq 2 ]] ; then
+    if [ -f "$1" ] ; then
+      commit_file="$1"
+      commit_message="$2"
+    elif [ -f "$2" ] ; then
+      commit_file="$2"
+      commit_message="$1"
+    else
+      echo "Wrong file"
+      echo "$usage"
+      return 1
+    fi
+  fi
+
+  echo -e "\n${Blinking_White}Updating git Repo${Color_Off}\n"
+
+  echo -e "Commit message will be ${commit_message}\n"
+  read -p "Hit \"Enter\" to continue or \"Ctrl+c\" to quit: "
+
   # add local changes
   if (( $(git status -s | wc -l) > 0  )) ; then
     echo -e "\n${UYellow}Adding modified files${Color_Off}"
@@ -41,7 +70,7 @@ git-update ()
     sleep .5
 
     echo -e "\n${UYellow}Comitting changes${Color_Off}"
-    git commit -m "$(date)" -v || { echo -e "${Red}Failed${Color_Off}" ; return 1 ; }
+    git commit -m "$commit_message" -v "$commit_file" || { echo -e "${Red}Failed${Color_Off}" ; return 1 ; }
   else
     echo -e "\n${UGreen}No local modified files to add"
   fi
