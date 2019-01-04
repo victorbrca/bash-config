79dee9c55f3465586fd4279b82402344c216c572
victorbrca <victorbrca@yahoo.ca>
2019-01-04 16:46:10 -0500

    Initial

---
+++ plugins/vagrant.bash
@@ -0,0 +1,24 @@
+#
+## about:Vagrant aliases
+#
+
+# help:vagup:vagrant up
+alias vagup='vagrant up'
+
+# help:vagdest:vagrant destroy
+alias vagdest='vagrant destroy'
+
+# help:vagdestf:vagrant destroy -f
+alias vagdestf='vagrant destroy -f'
+
+# help:vagstat:vagrant status
+alias vagstat='vagrant status'
+
+# help:vagssh:vagrant ssh
+alias vagssh='vagrant ssh'
+
+# help:vaghalt:vagrant halt
+alias vaghalt='vagrant halt'
+
+# help:vagsusp:vagrant suspend
+alias vagsusp='vagrant suspend'
