df3a5a8aff2ba0d52cb32c406375452276306a6f
victorbrca <victorbrca@yahoo.ca>
2019-01-22 12:34:14 -0500

    Adding SSH and root indicator

--- themes/simple-git.theme.bash
+++ themes/simple-git.theme.bash
@@ -6,11 +6,18 @@
 
 . ${bash_config_themes_folder}/lib/git-status.theme.bash
 
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-  export PS1="\h:\w \`if [ \$? = 0 ]; then echo ${green_color}✓${reset_color}; else echo ${red_color}✗${reset_color}; fi\` \$ "
+# Sets up the prompt for SSH and root
+if [[ $(id -u) -eq 0  && (-n "$SSH_CLIENT" || -n "$SSH_TTY") ]] ; then
+  ps1_header="\u\h:\w"
+elif [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] ; then
+  ps1_header="\u@\h"
+elif [[ $(id -u) -eq 0 ]] ; then
+  ps1_header="\u:\w"
 else
-  export PS1="\w \`_git_branch\`\`if [ \$? = 0 ]; then echo ${green_color}✓${reset_color}; else echo ${red_color}✗${reset_color}; fi\` \$ "
+  ps1_header="\w"
 fi
+
+export PS1="${ps1_header} \`_git_branch\`\`if [ \$? = 0 ]; then echo ${green_color}✓${reset_color}; else echo ${red_color}✗${reset_color}; fi\` \$ "
 
 export PS2=">> "
 export PS4="++ "
