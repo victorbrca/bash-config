1561ecc7e118390d81e56c57cb8446ab641ac180
victorbrca <victorbrca@yahoo.ca>
2019-02-01 15:30:04 -0500

    Adding systemd aliases

--- plugins/systemd.plugin.bash
+++ plugins/systemd.plugin.bash
@@ -2,40 +2,88 @@
 ## about:SystemD aliases
 #
 
-if command -v systemctl > /dev/null ; then
-  # help:sc:systemctl
-  alias sc='systemctl'
-  
-  # help:scr:systemctl restart
-  alias scr='systemctl restart'
-  
-  # help:scdr:systemctl daemon-reload
-  alias scdr='systemctl daemon-reload'
-  
-  # help:scu:systemctl --user
-  alias scu='systemctl --user'
-  
-  # help:scur:systemctl --user restart
-  alias scur='systemctl --user restart'
-  
-  # help:scudr:systemctl --user daemon-reload
-  alias scudr='systemctl --user daemon-reload'
-  
-  # help:scst:systemctl status
-  alias scst='systemctl status'
-  
-  # help:scust:systemctl --user status
-  alias scust='systemctl --user status'
-  
-  # help:sce:systemctl stop
-  alias sce='systemctl stop'
-  
-  # help:scue:systemctl --user stop
-  alias scue='systemctl --user stop'
-  
-  # help:scs:systemctl start
-  alias scs='systemctl start'
+# Other aliases options
+# https://github.com/lzap/systemd-shortcuts
 
-  # help:scus:systemctl --user start
-  alias scus='systemctl --user start'
-fi
+# if command -v systemctl > /dev/null ; then
+#   # help:sc:systemctl
+#   alias sctl='systemctl'
+  
+#   # help:scst:systemctl status
+#   alias sctlstat='systemctl status'
+  
+#   # help:scs:systemctl start
+#   alias sctlsta='systemctl start'
+
+#   # help:sce:systemctl stop
+#   alias sctlstp='systemctl stop'
+
+#   # help:scr:systemctl restart
+#   alias sctlrest='systemctl restart'
+
+#   # help:scdr:systemctl daemon-reload
+#   alias sctlrel='systemctl daemon-reload'
+
+#   # help:scu:systemctl --user
+#   alias sctlu='systemctl --user'
+  
+#   # help:scust:systemctl --user status
+#   alias sctlustat='systemctl --user status'
+  
+#   # help:scus:systemctl --user start
+#   alias sctlusta='systemctl --user start'
+  
+#   # help:scue:systemctl --user stop
+#   alias sctlustp='systemctl --user stop'
+
+#   # help:scur:systemctl --user restart
+#   alias sctlurest='systemctl --user restart'
+  
+#   # help:scudr:systemctl --user daemon-reload
+#   alias sctlurel='systemctl --user daemon-reload'
+# fi
+
+#help:sc:systemctl {user}[service] [start|stop|restart|status|enable|disable]
+sc ()
+{
+  usage="Usage: sctl {user}[service] [start|stop|restart|status|enable|disable]"
+
+  if [[ "$1" == "user" ]] ; then
+    service="$2"
+    action="$3"
+  else
+    service="$1"
+    action="$2"
+  fi
+
+  systemctl "$action" "$service"
+}
+
+
+# Bach completion for sc
+__listSystemdServices ()
+{
+  local cur
+  cur=${COMP_WORDS[COMP_CWORD]}
+
+  if [[ "${COMP_WORDS[1]}" = "user" &&  ! "${COMP_WORDS[3]}" || "${COMP_WORDS[1]}" = "$cur" ]] ; then
+    case ${COMP_WORDS[1]} in
+      user)
+        file_list="$(ls /lib/systemd/user/*.service ${HOME}/.config/systemd/user/*.service 2> /dev/null | awk -F"/" '{print $NF}')"
+        ;;
+      *)  
+        file_list="$(ls /lib/systemd/system/*.service /etc/systemd/system/*.service 2> /dev/null | awk -F"/" '{print $NF}')"
+        ;;
+    esac
+
+    COMPREPLY=($(compgen -W "$file_list" -- "${cur}"))
+    set +x
+    return 0
+  elif [[ "${COMP_WORDS[3]}" = "$cur" || "${COMP_WORDS[2]}" = "$cur" ]] ; then
+    COMPREPLY=($(compgen -W "start stop restart status enable disable" "$cur"))
+    set +x
+    return 0
+  fi  
+}
+
+complete -F __listSystemdServices -o filenames sc
