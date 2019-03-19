651b327f75d8c718d6c2ad2bcd0df7df497862da
victorbrca <victorbrca@yahoo.ca>
2019-03-19 09:47:33 -0400

    Adding yay as an AUR option

---
+++ plugins/yay.plugin.bash
@@ -0,0 +1,66 @@
+#
+## about:Aliases for yay 
+#
+
+# help:aur:Handler for yay
+aur ()
+{
+  local usage aur_log package update_count
+
+  usage="aur {install|update|upgrade|search|info}"
+
+  if [ -d "${HOME}/bin/var/log" ] ; then
+    aur_log=${HOME}/bin/var/log/$(hostname)-pacman-install.log
+  else
+    aur_log="/dev/null"
+  fi
+
+  case $1 in
+      install)
+        shift
+        /usr/bin/yay -Sy "$@"
+        if [[ $? -eq 0 ]] ; then
+          echo -e "\n** Logging installed packages"
+          for package in "$@" ; do
+            echo -e "\t- Logging $package... \c"
+            grep -q "$package" "$aur_log"
+            if [[ $? -eq 0 ]] ; then
+              echo "Already logged"
+            else
+              echo -e "$(date)\t-\tInstalled package: $package" >> "$aur_log"
+              echo "ok"
+            fi
+          done
+        fi
+        ;;
+      search)
+        shift
+        for package in "$@" ; do
+          /usr/bin/yay -Ssa "$package"
+        done
+        ;;
+      info) shift ; /usr/bin/yay -Sia mplayer "$@" ;;
+      update) 
+          /usr/bin/yay -Sya
+          update_count=$(yay -Qum | wc -l)
+          if (( update_count > 0 )) ; then
+            echo -e "\nThere are $update_count AUR packages waiting to be updated. Would you like to go ahead?"
+            read -p "[y|n]: " answr
+            case $answr in
+              y|Y|yes|YES) yes | /usr/bin/yay -Sua --answerupgrade none --answerclean all --answerdiff none --answeredit none ;;
+            esac
+          fi
+          ;;
+      upgrade) shift ; yes | /usr/bin/yay -Syua --answerupgrade none --answerclean all --answerdiff none --answeredit none ;;
+      *) 
+        echo "Invalid option"
+        echo "$usage"
+        return 1
+        ;;
+  esac
+}
+
+# help:checkupdates-aur:Shows available updates for aur
+alias checkupdates-aur='yay -Qum'
+
+complete -W 'install update search list remove upgrade' aur
