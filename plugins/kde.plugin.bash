0304e4420889034869d2840236ae4a4f346f6b43
victorbrca <victorbrca@yahoo.ca>
2020-10-09 12:56:41 -0400

    Adding restartlatte

--- plugins/kde.plugin.bash
+++ plugins/kde.plugin.bash
@@ -4,3 +4,6 @@
 
 # help:restartkde:Restarts plasmashell
 alias restartkde='killall plasmashell ; sleep 2 ; kstart5 plasmashell'
+
+# help:restartlatte:Restarts latte-dock
+alias restartlatte='killall latte-dock ; sleep 2 ; latte-dock &'
