1657954ec05e2b4c757a34317e271aac9a5b928c
victorbrca <victorbrca@yahoo.ca>
2020-12-28 13:11:49 -0500

    Added aur_builder user for passwordless update of aur packages

--- plugins/yay.plugin.bash
+++ plugins/yay.plugin.bash
@@ -9,16 +9,24 @@ aur ()
 
   usage="aur {install|update|upgrade|search|info}"
 
+  # Setup log folder
   if [ -d "${HOME}/bin/var/log" ] ; then
     aur_log=${HOME}/bin/var/log/$(hostname)-pacman-install.log
   else
     aur_log="/dev/null"
   fi
 
+  # Checks if we are using a aur_builder user
+  if [ $(id aur_builder &> /dev/null ; echo $?) -eq 0 ] ; then
+    command_yay="/usr/bin/sudo -u aur_builder /usr/bin/yay"
+  else
+    command_yay="/usr/bin/yay"
+  fi
+
   case $1 in
       install)
         shift
-        /usr/bin/yay -Say --answerupgrade none --answerclean all --answerdiff none --answeredit none "$@"
+        $command_yay -Say --answerupgrade none --answerclean all --answerdiff none --answeredit none "$@"
         if [[ $? -eq 0 ]] ; then
           echo -e "\n** Logging installed packages"
           for package in "$@" ; do
@@ -36,27 +44,27 @@ aur ()
       search)
         shift
         for package in "$@" ; do
-          /usr/bin/yay -Ssa --sortby votes --topdown "$package"
+          $command_yay -Ssa --sortby votes --topdown "$package"
         done
         ;;
-      info) shift ; /usr/bin/yay -Sia mplayer "$@" ;;
+      info) shift ; $command_yay -Sia mplayer "$@" ;;
       update)
           if [[ $# -gt 1 ]] ; then
             shift
-            /usr/bin/yay -Sa --answerupgrade none --answerclean none --answerdiff none --answeredit none "$@"
+            $command_yay -Sa --answerupgrade none --answerclean none --answerdiff none --answeredit none "$@"
           else
-            /usr/bin/yay -Sya
+            $command_yay -Sya
             update_count=$(yay -Qum | wc -l)
             if (( update_count > 0 )) ; then
               echo -e "\nThere are $update_count AUR packages waiting to be updated. Would you like to go ahead?"
               read -p "[y|n]: " answr
               case $answr in
-                y|Y|yes|YES) yes | /usr/bin/yay -Sua --answerupgrade none --answerclean none --answerdiff none --answeredit none ;;
+                y|Y|yes|YES) yes | $command_yay -Sua --answerupgrade none --answerclean none --answerdiff none --answeredit none ;;
               esac
             fi
           fi
           ;;
-      upgrade) shift ; yes | /usr/bin/yay -Syua --answerupgrade none --answerclean none --answerdiff none --answeredit none ;;
+      upgrade) shift ; yes | $command_yay -Syua --answerupgrade none --answerclean none --answerdiff none --answeredit none ;;
       *) 
         echo "Invalid option"
         echo "$usage"
