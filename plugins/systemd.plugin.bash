a9e0b944aeb5fffec78d8167efc85a51f5225226
victorbrca <victorbrca@yahoo.ca>
2020-12-28 13:08:29 -0500

    Added fzf to the systemd alias

--- plugins/systemd.plugin.bash
+++ plugins/systemd.plugin.bash
@@ -2,47 +2,164 @@
 ## about:SystemD aliases
 #
 
-# help:sc:systemctl {user}[service] [start|stop|restart|status|enable|disable]
+if ! command -v fzf > /dev/null ; then
+  echo "[bash-config: systemd] fzf is not installed"
+  return 0
+fi
+
+##Not currently enable
+  # link
+  # revert
+  # add-wants
+  # add-requires
+
+systemd_svc_opts="
+cat
+disable
+edit
+enable
+force-reload
+is-active
+is-enabled
+is-failed
+list-dependencies
+mask
+reenable
+reload
+reload-or-restart
+restart
+show
+start
+status
+stop
+try-reload-or-restart
+try-restart
+unmask
+"
+
+systemd_cmd_opts="
+cancel
+condreload
+condrestart
+condstop
+daemon-reexec
+daemon-reload
+default
+emergency
+exit
+get-default
+halt
+help
+hibernate
+hybrid-sleep
+import-environment
+isolate
+is-system-running
+kexec
+kill
+list-jobs
+list-machines
+list-sockets
+list-timers
+list-unit-files
+list-units
+log-level
+log-target
+poweroff
+preset
+preset-all
+reboot
+rescue
+reset-failed
+revert
+service
+service-watchdogs
+set-default
+set-environment
+set-property
+show-environment
+suspend
+suspend-then-hibernate
+switch-root
+unset-environment"
+
+_list_cmd_opts_sc () {
+  local cur selctd_svc selctd_svc_opt
+  cur=${COMP_WORDS[COMP_CWORD]}
+  case $COMP_CWORD in
+    1)
+      COMPREPLY=($(compgen -W "$(echo "$systemd_cmd_opts")" "$cur"))
+      ;;
+    2) 
+      if [[ "${COMP_WORDS[1]}" == "service" ]] ; then
+        if [[ ! "${COMP_WORDS[2]}" ]] ; then
+          selctd_svc=$({ systemctl --system list-unit-files --type=service --no-legend | awk '{print $0 "\t" "system"}' ;
+           systemctl --user list-unit-files --type=service --no-legend | awk '{print $0 "\t" "user"}' ; } \
+           | fzf -e --reverse --tiebreak=index)
+        elif [[ "${COMP_WORDS[2]}" ]] ; then
+          selctd_svc=$({ systemctl --system list-unit-files --type=service --no-legend | awk '{print $0 "\t" "system"}' ;
+           systemctl --user list-unit-files --type=service --no-legend | awk '{print $0 "\t" "user"}' ; } \
+           | fzf -q "${COMP_WORDS[2]}" -e --reverse --tiebreak=index)
+        fi
+        selctd_svc=$(echo $selctd_svc | awk '{print $1 , $NF}')
+      fi
+      selctd_svc_opt=$(echo "$systemd_svc_opts" | fzf -e --reverse --tiebreak=index)
+      COMPREPLY="$selctd_svc $selctd_svc_opt"
+      ;;
+    esac
+}
+
+#help:sc:Systemd fzf wrapper
 sc ()
 {
-  usage="Usage: sc {user}[service] [start|stop|restart|status|enable|disable]"
+  local cmd usage elevated_cmds
+  usage="Usage: sc [cmd {user}|service [unit] [system|user] [unit command] {now}]
+examples:
+\tsc service cupsd.service system restart
+\tsc service mpd.service user restart
+\tsc service mpd.service enable now user
+\tsc daemon-reload
+\tsc daemon-reload user
+"
 
-  if [[ "$1" == "user" ]] ; then
-    service="$2"
-    action="$3"
-    systemctl --user "$action" "$service"
-  else
-    service="$1"
-    action="$2"
-    systemctl "$action" "$service"
-  fi
+  elevated_cmds='(start|stop|restart|enable|disable|reenable|try-restart|reload|force-reload|try-reload-or-restart|try-restart|mask|unmask)'
+
+  case $1 in
+    service)
+      case $3 in
+        user) cmd="systemctl --user" ;;
+        system)
+          if [[ $(id -u) -ne 0 ]] ; then
+            if [[ $4 =~ $elevated_cmds ]] ; then
+              cmd="sudo systemctl"
+            else
+              cmd="systemctl"
+            fi
+          else
+            cmd="systemctl"
+          fi
+          ;;
+      esac
+
+      if [[ "$5" == "now" ]] ; then
+        cmd_arg="--now"
+      fi
+
+      $cmd $4 $cmd_arg $2
+
+      if [[ $4 =~ $elevated_cmds ]] ; then
+        echo showing status
+        sleep .5
+        systemctl status $2 --no-pager
+      fi
+      ;;
+    -h)
+      echo -e "$usage"
+      ;;
+    *)
+      $cmd $2 
+      ;;
+  esac
 }
 
-
-# Bach completion for sc
-__listSystemdServices ()
-{
-  local cur
-  cur=${COMP_WORDS[COMP_CWORD]}
-
-  if [[ "${COMP_WORDS[1]}" = "user" &&  ! "${COMP_WORDS[3]}" || "${COMP_WORDS[1]}" = "$cur" ]] ; then
-    case ${COMP_WORDS[1]} in
-      user)
-        file_list="$(ls /lib/systemd/user/*.service ${HOME}/.config/systemd/user/*.service 2> /dev/null | awk -F"/" '{print $NF}')"
-        ;;
-      *)  
-        file_list="$(ls /lib/systemd/system/*.service /etc/systemd/system/*.service 2> /dev/null | awk -F"/" '{print $NF}')"
-        ;;
-    esac
-
-    COMPREPLY=($(compgen -W "$file_list" -- "${cur}"))
-    set +x
-    return 0
-  elif [[ "${COMP_WORDS[3]}" = "$cur" || "${COMP_WORDS[2]}" = "$cur" ]] ; then
-    COMPREPLY=($(compgen -W "start stop restart status enable disable" "$cur"))
-    set +x
-    return 0
-  fi  
-}
-
-complete -F __listSystemdServices -o filenames sc
+complete -F _list_cmd_opts_sc -o default -o bashdefault sc
