966282fb8bbd2a369aa52a686aeea2d9dea4924a
victorbrca <victorbrca@yahoo.ca>
2021-03-08 13:09:41 -0500

    Adding gitgraph

--- plugins/git.plugin.bash
+++ plugins/git.plugin.bash
@@ -14,6 +14,14 @@ alias gitcmt='git commit'
 # help:gtcmtdate:git commit -m "$(date)"
 alias gitcmtdate='git commit -m "`date`"'
 
+# help:gitgraph:Shows commit graph
+gitgraph ()
+{
+  git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) \
+- %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- \
+%an%C(reset)%C(bold yellow)%d%C(reset)' --all
+}
+
 # help:gitpushom:git push -u origin master
 gitpushom ()
 {
